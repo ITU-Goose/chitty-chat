2a2c512f852c47950be6672fef83c0132064d4b4
AmalieBoegild <abso@itu.dk>
2021-10-26 10:49:39 +0200

    refactor

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"os"
 
+	"github.com/google/uuid"
 	pb "github.com/goose-alt/chitty-chat/api/v1/pb/chat"
 	ts "github.com/goose-alt/chitty-chat/internal/time"
 	"google.golang.org/grpc"
@@ -20,8 +21,9 @@ const (
 )
 
 func main() {
-
-	timestamp := ts.CreateVectorTimestamp("abe")
+	// Generate a new uuid for the client
+	id := uuid.New().String()
+	timestamp := ts.CreateVectorTimestamp(id)
 
 	// Set up a connection to the server.
 	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
@@ -35,9 +37,14 @@ func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	message := readInput()
+	waitc := make(chan struct{})
+	stream, err := c.Chat(context.Background())
+	if err!=nil{
+		return
+	}
 
-	chat(c,ctx,message,timestamp)
+	uuid,name := register(c,ctx,timestamp,stream,waitc)
+	chat(c,ctx,timestamp,stream,waitc,uuid,name)
 
 }
 
@@ -46,12 +53,41 @@ func readInput() string {
 	return input
 }
 
-func chat(c pb.ChatClient, ctx context.Context, message string, timestamp ts.VectorTimestamp) {
-	stream, err := c.Chat(context.Background())
-	if err!=nil{
+func register(c pb.ChatClient, ctx context.Context, timestamp ts.VectorTimestamp, stream pb.Chat_ChatClient, waitc chan struct{}) (userId string, name string) {
+	name = askForUsername()
+
+	timestamp.Increment()
+
+	stream.Send(&pb.Message{Content: "",Timestamp: &pb.Lamport{Clients: timestamp.GetVectorTime()}, Info: &pb.ClientInfo{Uuid: "", Name: name}})
+	in, err := stream.Recv()
+	if err == io.EOF {
+		// read done.
+		close(waitc)
 		return
 	}
-	waitc := make(chan struct{})
+	if err != nil {
+		log.Fatalf("Failed to receive a note : %v", err)
+	}
+	userId = in.Info.Uuid
+	timestamp.Sync(in.Timestamp.Clients)
+	log.Printf("Got message %s. From: %s. Timestamp: %s", in.Content, in.Info.Name, timestamp.GetDisplayableContent())
+	  
+	return userId,name		
+}
+
+func askForUsername() string {
+	msg := "Enter your username"
+	fmt.Println(msg)
+	fmt.Print("-> ")
+
+	reader := bufio.NewReader(os.Stdin)
+	username, _ := reader.ReadString('\n')
+
+	return username
+}
+
+func chat(c pb.ChatClient, ctx context.Context, timestamp ts.VectorTimestamp, stream pb.Chat_ChatClient, waitc chan struct{}, uuid string, name string) {
+	message := readInput()
 	go func() {
 		for {
 			in, err := stream.Recv()
@@ -61,14 +97,14 @@ func chat(c pb.ChatClient, ctx context.Context, message string, timestamp ts.Vec
 				return
 			}
 			if err != nil {
-				log.Fatalf("Failed to receive a note : %v", err)
+				log.Fatalf("Failed to receive a message: %v", err)
 			}
 			timestamp.Sync(in.Timestamp.Clients)
 			timestamp.Increment()
 		  log.Printf("Got message %s. From: %s. Timestamp: %s", in.Content, in.Info.Name, timestamp.GetDisplayableContent())
 		}
 	  }()
-	  mes := pb.Message{Content: message,Timestamp: &pb.Lamport{Clients: timestamp.GetVectorTime()}, Info: &pb.ClientInfo{Uuid: "", Name: "Amalie"}}
+	  mes := pb.Message{Content: message,Timestamp: &pb.Lamport{Clients: timestamp.GetVectorTime()}, Info: &pb.ClientInfo{Uuid: uuid, Name: name}}
 		if err := stream.Send(&mes); err != nil {
 		  log.Fatalf("Failed to send a note: %v", err)
 		}
